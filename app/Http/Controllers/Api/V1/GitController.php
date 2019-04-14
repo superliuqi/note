4ad992b6173c80946bcaf817cb8f3ef93d37c1bc
刘琪 <liuqi@192.168.0.107>
2019-04-14 10:32:23 +0800

    git

---
+++ app/Http/Controllers/Api/V1/GitController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+
+class GitController extends Controller
+{
+
+    public function index() {
+        file_put_contents('./git.txt', file_get_contents("php://input"));
+    }
+}
