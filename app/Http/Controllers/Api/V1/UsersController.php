d3cb932f8a26fb7d5e581ec03200a4101201ab6c
刘琪 <359380474@qq.com>
2019-04-26 14:36:22 +0800

    test

--- app/Http/Controllers/Api/V1/UsersController.php
+++ app/Http/Controllers/Api/V1/UsersController.php
@@ -16,7 +16,7 @@ class UsersController extends Controller
 
     public function users()
     {
-        dd($this->wechat->user->list());
+        dd($this->wechat->auto_reply->current());
     }
 
 }
