a9e7a2af9d8462fdc177ac8788df8b98edfff996
刘琪 <359380474@qq.com>
2019-04-26 13:31:13 +0800

    wechat

--- app/Http/Controllers/Api/V1/WechatController.php
+++ app/Http/Controllers/Api/V1/WechatController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use log;
 class WechatController extends Controller
 {
     /**
@@ -13,7 +12,7 @@ class WechatController extends Controller
      */
     public function serve()
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+//        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
         $app->server->push(function($message){
