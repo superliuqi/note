9fd69fc3966cbd2160ad6908baf7e9ab419bc67f
刘琪 <359380474@qq.com>
2019-04-26 13:47:59 +0800

    wechat

--- app/Http/Controllers/Api/V1/WechatController.php
+++ app/Http/Controllers/Api/V1/WechatController.php
@@ -15,9 +15,45 @@ class WechatController extends Controller
 //        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
+//        $app->server->push(function($message){
+//            return "欢迎关注 overtrue！";
+//        });
+
+
+        $app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '我收到了，收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+
+            // ...
         });
+
 
         return $app->server->serve();
     }
