2eca1010d482a3180cf43aa2023b15074acca11e
刘琪 <359380474@qq.com>
2019-04-29 15:17:58 +0800

    api

--- app/Http/Controllers/Api/V1/IndexController.php
+++ app/Http/Controllers/Api/V1/IndexController.php
@@ -9,6 +9,9 @@ class IndexController extends Controller
 {
 
     public function index(Request $request) {
-        echo $request->cityName;exit;
+        $cityName =  $request->cityName;
+
+        $result = file_get_contents('http://api.map.baidu.com/geocoder?address='.$cityName.'&output=json&key=37492c0ee6f924cb5e934fa08c6b1676&city=%E5%8C%97%E4%BA%AC%E5%B8%82');
+        return $result;
     }
 }
