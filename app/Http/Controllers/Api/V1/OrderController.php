fb2672a95490433fc81c35694a0122fa26e942cc
刘琪 <359380474@qq.com>
2019-09-18 11:23:25 +0800

    111

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Jobs\Order;
 use App\Jobs\ProcessPodcast;
 use App\Models\Test;
+use App\Models\TestOrder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
@@ -26,9 +27,12 @@ class OrderController extends Controller
 		];
 
 		try {
-//			$orderList = 'order:temp:list';
+			$orderList = 'order:temp:list';
 //			$result = Redis::lpush($orderList,json_encode($order));
 			$result = Order::dispatch($order)->onQueue('order')->delay(now()->addMinutes(mt_rand(1, 3)));
+			$key    = $orderList . ':' . $order['order_id'];
+			Redis::set($key, json_encode($order));
+			Redis::expire($key, 300);
 			if ($result) {
 				echo '下单成功';
 				exit;
@@ -39,5 +43,25 @@ class OrderController extends Controller
 		} catch (\Exception $e) {
 
 		}
+	}
+
+	public function pay(Request $request)
+	{
+		$order_id = $request->order_id;
+		if (!$order_id) {
+			exit('fail');
+		}
+		$redis_key = 'order:temp:list';
+
+		$exists = Redis::get($redis_key . ':' . $order_id);
+		if (!$exists) {
+			exit('已过期');
+		}
+		$ex = TestOrder::where('order_id', $order_id)->first();
+		if (!$ex) {
+			exit('fail_2');
+		}
+		TestOrder::where('order_id', $order_id)->update(['status' => 1]);
+		echo 'success';
 	}
 }
